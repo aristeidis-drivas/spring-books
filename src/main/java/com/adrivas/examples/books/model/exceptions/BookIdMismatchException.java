cbb33c9cb3a500e44f16f8de2bf4970818a5b55f
aristeidis-drivas <aristeidisdrivas@gmail.com>
2022-02-12 15:51:58 +0200

    RestAssure for controllers testing + Exception handling

---
+++ src/main/java/com/adrivas/examples/books/model/exceptions/BookIdMismatchException.java
@@ -0,0 +1,23 @@
+package com.adrivas.examples.books.model.exceptions;
+
+/**
+ * BookIdMismatchException model.
+ */
+public class BookIdMismatchException extends RuntimeException {
+
+    public BookIdMismatchException() {
+        super();
+    }
+
+    public BookIdMismatchException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    public BookIdMismatchException(final String message) {
+        super(message);
+    }
+
+    public BookIdMismatchException(final Throwable cause) {
+        super(cause);
+    }
+}
